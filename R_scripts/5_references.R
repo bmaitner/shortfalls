c9e9db44bdc7f5a9d37a6482595cbb2300a7836a
Brian <bmaitner@gmail.com>
2023-03-15 12:18:11 -0400

    modified references code

--- R_scripts/5_references.R
+++ R_scripts/5_references.R
@@ -1,51 +1,43 @@
-
-traits <- arrow::open_dataset(sources = "manual_downloads/TRY/TRY_parquet/")
-
-traits %>%
-  dplyr::select(Reference)%>%
-  collect()%>%
-  unique() -> try_refs
-
-austraits <- austraits::load_austraits(path = "data/austraits/",
-                                       version = "3.0.2")
-
-aus_refs <- austraits$sources
+library(tidyverse)
 
 
-citation("parzer")
+#Make sure we get all R packages
 
-citation("GNRS")
+  get_citations <- function(directory, out_file = NULL){
 
-#Make sure we get all R functions
+    packages <-
+    list.files(path = directory,
+               recursive = TRUE,
+               pattern = ".R$",
+               full.names = TRUE) %>%
+    questionr::qscan(load = FALSE) %>%
+      unlist() %>%
+      as.vector()%>%
+      unique()
 
-# get_citations <- function(directory, out_file = NULL){
-#
-#   packages <-
-#   list.files(path = directory,
-#              recursive = TRUE,
-#              pattern = ".R$",
-#              full.names = TRUE) %>%
-#   questionr::qscan(load = FALSE) %>%
-#     unlist() %>%
-#     as.vector()
-#
-#
-#   out <- sapply(packages,
-#          FUN = function(x){
-#
-#           cite <- tryCatch(expr = toBibtex(citation(x)),
-#                    error = function(e){})
-#
-#           if(!is.null(cite) & is.null(out_file)){
-#             write(x = cite,
-#                   file = out_file,
-#                   append = TRUE)
-#             }
-#
-#
-#          })
-#
-#   return(out)
-#
-#
-# }
+
+    out <- sapply(packages,
+           FUN = function(x){
+
+            cite <- tryCatch(expr = toBibtex(citation(x)),
+                     error = function(e){})
+
+            if(!is.null(cite) & !is.null(out_file)){
+              write(x = cite,
+                    file = out_file,
+                    append = TRUE)
+              }
+
+
+           })
+
+
+    return(out)
+
+
+  }
+
+
+get_citations(directory = getwd(),
+              out_file = "data/r_packages_used.bib")
+
